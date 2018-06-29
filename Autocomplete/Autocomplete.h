ed5ab51018a4d821e7efdeb10659c75efddcac94
Yigitcan Yurtsever <yigitcan.yurtsever@gmail.com>
2018-06-29 12:41:52 -0700

    Create project

---
+++ Autocomplete/Autocomplete.h
@@ -0,0 +1,14 @@
+//
+//  Autocomplete.h
+//  Autocomplete
+//
+//  Created by Yigitcan Yurtsever on 29.06.2018.
+//  Copyright © 2018 Zeplin, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+FOUNDATION_EXPORT double AutocompleteVersionNumber;
+FOUNDATION_EXPORT const unsigned char AutocompleteVersionString[];
+
+#import <Autocomplete/ZPLAutocompletePluginController.h>`
---
+++ Autocomplete/Sources/ZPLAutocompletePluginController.m
@@ -0,0 +1,13 @@
+//
+//  ZPLAutocompletePluginController.m
+//  Autocomplete
+//
+//  Created by Yigitcan Yurtsever on 29.06.2018.
+//  Copyright © 2018 Zeplin, Inc. All rights reserved.
+//
+
+#import "ZPLAutocompletePluginController.h"
+
+@implementation ZPLAutocompletePluginController
+
+@end
