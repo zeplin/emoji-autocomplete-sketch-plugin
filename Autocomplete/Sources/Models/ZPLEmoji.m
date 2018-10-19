234deca05c0415c0aa1d1f7f7c81e810242aafa6
Yigitcan Yurtsever <yigitcan.yurtsever@gmail.com>
2018-10-19 16:22:40 -0700

    Check emoji tags when finding suggestions

--- Autocomplete/Sources/Models/ZPLEmoji.m
+++ Autocomplete/Sources/Models/ZPLEmoji.m
@@ -30,12 +30,12 @@ NSString * const ZPLEmojiAliasSeparator = @"_";
 
 static NSString * const ZPLEmojiValueDictionaryKey = @"emoji";
 static NSString * const ZPLEmojiAliasesDictionaryKey = @"aliases";
-static NSString * const ZPLEmojiDescriptionDictionaryKey = @"description";
+static NSString * const ZPLEmojiTagsDictionaryKey = @"tags";
 static NSString * const ZPLEmojiUnicodeVersionDictionaryKey = @"unicode_version";
 static NSString * const ZPLEmojiCategoryDictionaryKey = @"category";
 static NSString * const ZPLEmojiFlagsCategoryDictionaryValue = @"Flags";
 
-static NSString * const ZPLEmojiWhitespaceSeparator = @" ";
+static NSString * const ZPLEmojiFlagTag = @"flag";
 
 static const NSOperatingSystemVersion ZPLEmojiUnicode8OperationSystemVersion = {.majorVersion = 10, .minorVersion = 11, .patchVersion = 5};
 static const NSOperatingSystemVersion ZPLEmojiUnicode9OperationSystemVersion = {.majorVersion = 10, .minorVersion = 12, .patchVersion = 2};
@@ -64,28 +64,26 @@ static const NSOperatingSystemVersion ZPLEmojiUnicode9OperationSystemVersion = {
 
     NSString *value = (NSString *)[dictionary objectForKey:ZPLEmojiValueDictionaryKey];
     NSArray<NSString *> *aliases = (NSArray<NSString *> *)[dictionary objectForKey:ZPLEmojiAliasesDictionaryKey];
+    NSArray<NSString *> *tags = (NSArray<NSString *> *)[dictionary objectForKey:ZPLEmojiTagsDictionaryKey];
     NSString *unicodeVersion = (NSString *)[dictionary objectForKey:ZPLEmojiUnicodeVersionDictionaryKey];
 
     if (!value || !aliases || aliases.count == 0 || !unicodeVersion || ![ZPLEmoji supportsUnicodeVersion:unicodeVersion]) {
         return nil;
     }
 
+    if (!tags) {
+        tags = [NSArray array];
+    }
+
     NSString *category = (NSString *)[dictionary objectForKey:ZPLEmojiCategoryDictionaryKey];
 
-    if ([category isEqualToString:ZPLEmojiFlagsCategoryDictionaryValue]) {
-        NSString *description = (NSString *)[dictionary objectForKey:ZPLEmojiDescriptionDictionaryKey];
-
-        if (description != nil) {
-            NSString *adjustedDescription = [[description stringByReplacingOccurrencesOfString:ZPLEmojiWhitespaceSeparator withString:ZPLEmojiAliasSeparator] lowercaseString];
-
-            if (![aliases containsObject:adjustedDescription]) {
-                aliases = [aliases arrayByAddingObject:adjustedDescription];
-            }
-        }
+    if ([category isEqualToString:ZPLEmojiFlagsCategoryDictionaryValue] && ![tags containsObject:ZPLEmojiFlagTag]) {
+        tags = [tags arrayByAddingObject:ZPLEmojiFlagTag];
     }
 
     _value = [value copy];
     _aliases = aliases;
+    _tags = tags;
 
     return self;
 }
